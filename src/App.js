7148e8e238b5b5d0be5c14d05e95b8f1715cb85c
Mohsin Miah <moemia12@gmail.com>
2023-02-13 22:00:58 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Guild Tech Redux Intro</p>
       </header>
+      <div className="counter-container">
+        <button
+          className="increment-button"
+          onClick={() => setCount(count + 1)}>
+          + 1
+        </button>
+        <div className="count-box">
+          <h3>{count}</h3>
+        </div>
+        <button
+          className="decrement-button"
+          onClick={() => setCount(count - 1)}>
+          - 1
+        </button>
+      </div>
     </div>
   );
 }
